0e2515f7c6b6687d6225b7e47d005efdee2ff700
Pavel Ershov <pave1ershov@yandex.ru>
2025-10-28 01:27:48 +0300

    ruff fix

--- nutrition/nutrition_trecker/models.py
+++ nutrition/nutrition_trecker/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.utils.translation import gettext_lazy as _
 from django.db.models import Q, F
 from django.db.models.functions import Now
 from django.core.exceptions import ValidationError
